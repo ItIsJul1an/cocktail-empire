2f1bd4ed8bcd8d312560fc45d090b7313dca7b0e
B-julia <julia.bodingbauer@gmx.at>
2022-11-10 11:43:27 +0100

    added Model

---
+++ cocktails/src/model/cocktail.ts
@@ -0,0 +1,14 @@
+export interface Cocktail {
+
+    strDrink: string,
+    strVideo?: string,
+    strAlcoholic: string, 
+    strGlass: string, 
+    strInstructions: string, 
+    strDrinkThumb: string, 
+    strIngredients: string[],
+    strMeasures: string[]
+
+
+
+}
