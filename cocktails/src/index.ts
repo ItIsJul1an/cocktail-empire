aa1ab8611177f9993f3dc1fd0c32b47887b75196
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-11 09:12:16 +0100

    Remove comment

--- cocktails/src/index.ts
+++ cocktails/src/index.ts
@@ -1,8 +1,6 @@
 import cocktailService from "./cocktail-service"
 import "./components/app-component"
 
-//cocktailService.fetchCocktails()
-
 const body= document.querySelector("body")
 const appComponent = document.createElement("app-component")
 body.appendChild(appComponent)
